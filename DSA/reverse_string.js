07c9b3a75f5c60f1ff1035c1947861227504a07b
spexzee <abub5979@gmail.com>
2023-12-29 23:34:05 +0530

    reverse string logic

--- DSA/reverse_string.js
+++ DSA/reverse_string.js
@@ -1,1 +1,11 @@
-
+const ReverseString = (str) => {
+    let arr = str.split("");
+    let revStr = "";
+    for (let i = arr.length - 1; i >= 0; i--) {
+        revStr+= arr[i];
+    }
+    return revStr;
+}
+let str = "Abu"
+ let result = ReverseString(str)
+console.log(result);
