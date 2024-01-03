71bbd68bbe196e4f50e5522177cb5042f84e7f04
spexzee <abub5979@gmail.com>
2024-01-03 14:51:57 +0530

    fibonacci

---
+++ DSA/fib.js
@@ -0,0 +1,11 @@
+function fibonacci(range) {
+    let fib = [0, 1];
+    for (let i = 2; i < range; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib;
+}
+
+console.log(fibonacci(5));
+console.log(fibonacci(7));
+console.log(fibonacci(9));
