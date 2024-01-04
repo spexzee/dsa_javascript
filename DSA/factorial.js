7f6cc282307f1507522f1facbec679460a856eee
Spexzee_Abu <104344646+spexzee@users.noreply.github.com>
2024-01-04 13:48:38 +0530

    Update factorial.js

--- DSA/factorial.js
+++ DSA/factorial.js
@@ -1,1 +1,11 @@
+function factorial(range) {
+    if(range === 0){
+      return 1;
+    }else{
+      return range * factorial(range - 1);
+    }
+}
 
+console.log(factorial(5));
+console.log(factorial(7));
+console.log(factorial(9));
